c5ac6cd1f2d5cde9388463b4e38e876049a9b0f1
Xin Wang <dram.wang@gmail.com>
2018-04-21 17:50:23 +0800

    No need to specify binding name explicitly

--- sources/sqlite.f90
+++ sources/sqlite.f90
@@ -6,39 +6,34 @@ module sqlite
   include "constants.f90"
 
   interface
-     function sqlite3_bind_text(stmt, index, text, bytes, destructor) &
-          bind(c, name="sqlite3_bind_text")
+     function sqlite3_bind_text(stmt, index, text, bytes, destructor) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: stmt, text, destructor
        integer(c_int), value :: index, bytes
        integer(c_int) sqlite3_bind_text
      end function sqlite3_bind_text
 
-     function sqlite3_close(db) &
-          bind(c, name="sqlite3_close")
+     function sqlite3_close(db) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: db
        integer(c_int) sqlite3_close
      end function sqlite3_close
 
-     function sqlite3_column_double(stmt, col) &
-          bind(c, name="sqlite3_column_double")
+     function sqlite3_column_double(stmt, col) bind(c)
        use iso_c_binding, only: c_double, c_int, c_ptr
        type(c_ptr), value :: stmt
        integer(c_int), value :: col
        real(c_double) sqlite3_column_double
      end function sqlite3_column_double
 
-     function sqlite3_column_int(stmt, col) &
-          bind(c, name="sqlite3_column_int")
+     function sqlite3_column_int(stmt, col) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: stmt
        integer(c_int), value :: col
        integer(c_int) sqlite3_column_int
      end function sqlite3_column_int
 
-     function sqlite3_column_text(stmt, col) &
-          bind(c, name="sqlite3_column_text")
+     function sqlite3_column_text(stmt, col) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: stmt
        integer(c_int), value :: col
@@ -51,23 +46,20 @@ module sqlite
        type(c_ptr) sqlite3_errmsg
      end function sqlite3_errmsg
 
-     function sqlite3_finalize(stmt) &
-          bind(c, name="sqlite3_finalize")
+     function sqlite3_finalize(stmt) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: stmt
        integer(c_int) sqlite3_finalize
      end function sqlite3_finalize
 
-     function sqlite3_open(filename, db) &
-          bind(c, name="sqlite3_open")
+     function sqlite3_open(filename, db) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: filename
        type(c_ptr), intent(out) :: db
        integer(c_int) sqlite3_open
      end function sqlite3_open
 
-     function sqlite3_prepare_v2(db, sql, bytes, stmt, tail) &
-          bind(c, name="sqlite3_prepare_v2")
+     function sqlite3_prepare_v2(db, sql, bytes, stmt, tail) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: db, sql
        integer(c_int), value :: bytes
@@ -75,8 +67,7 @@ module sqlite
        integer(c_int) sqlite3_prepare_v2
      end function sqlite3_prepare_v2
 
-     function sqlite3_step(stmt) &
-          bind(c, name="sqlite3_step")
+     function sqlite3_step(stmt) bind(c)
        use iso_c_binding, only: c_int, c_ptr
        type(c_ptr), value :: stmt
        integer(c_int) sqlite3_step
