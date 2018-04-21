f93b4b00d02f06f6c107a11bc422998256406d60
Xin Wang <dram.wang@gmail.com>
2018-04-21 19:56:43 +0800

    Add sqlite_done

--- sources/constants.c
+++ sources/constants.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+	printf("integer(c_int) :: sqlite_done = %d\n", SQLITE_DONE);
 	printf("integer(c_int) :: sqlite_error = %d\n", SQLITE_ERROR);
 	printf("integer(c_int) :: sqlite_ok = %d\n", SQLITE_OK);
 	printf("integer(c_int) :: sqlite_row = %d\n", SQLITE_ROW);
