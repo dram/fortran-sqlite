1cc65d05cf68ac996405c1dbf9e8d9b4009ed79f
Xin Wang <dram.wang@gmail.com>
2018-04-22 14:17:13 +0800

    Constants should be declared with parameter attribute.

--- sources/constants.c
+++ sources/constants.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
+#define INTEGER_PARAMETER "integer(c_int), parameter :: "
+
 int main(void)
 {
-	printf("integer(c_int) :: sqlite_done = %d\n", SQLITE_DONE);
-	printf("integer(c_int) :: sqlite_error = %d\n", SQLITE_ERROR);
-	printf("integer(c_int) :: sqlite_misuse = %d\n", SQLITE_MISUSE);
-	printf("integer(c_int) :: sqlite_ok = %d\n", SQLITE_OK);
-	printf("integer(c_int) :: sqlite_row = %d\n", SQLITE_ROW);
+	printf(INTEGER_PARAMETER "sqlite_done = %d\n", SQLITE_DONE);
+	printf(INTEGER_PARAMETER "sqlite_error = %d\n", SQLITE_ERROR);
+	printf(INTEGER_PARAMETER "sqlite_misuse = %d\n", SQLITE_MISUSE);
+	printf(INTEGER_PARAMETER "sqlite_ok = %d\n", SQLITE_OK);
+	printf(INTEGER_PARAMETER "sqlite_row = %d\n", SQLITE_ROW);
 
 	return 0;
 }
