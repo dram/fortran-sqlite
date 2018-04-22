509dc9eb6081d7e3e053033b2e098ab3f70a1125
Xin Wang <dram.wang@gmail.com>
2018-04-22 14:24:29 +0800

    Rename macro to match C type

--- sources/constants.c
+++ sources/constants.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
-#define INTEGER_PARAMETER "integer(c_int), parameter :: "
+#define INT_PARAMETER "integer(c_int), parameter :: "
 
 int main(void)
 {
-	printf(INTEGER_PARAMETER "sqlite_done = %d\n", SQLITE_DONE);
-	printf(INTEGER_PARAMETER "sqlite_error = %d\n", SQLITE_ERROR);
-	printf(INTEGER_PARAMETER "sqlite_misuse = %d\n", SQLITE_MISUSE);
-	printf(INTEGER_PARAMETER "sqlite_ok = %d\n", SQLITE_OK);
-	printf(INTEGER_PARAMETER "sqlite_row = %d\n", SQLITE_ROW);
+	printf(INT_PARAMETER "sqlite_done = %d\n", SQLITE_DONE);
+	printf(INT_PARAMETER "sqlite_error = %d\n", SQLITE_ERROR);
+	printf(INT_PARAMETER "sqlite_misuse = %d\n", SQLITE_MISUSE);
+	printf(INT_PARAMETER "sqlite_ok = %d\n", SQLITE_OK);
+	printf(INT_PARAMETER "sqlite_row = %d\n", SQLITE_ROW);
 
 	return 0;
 }
